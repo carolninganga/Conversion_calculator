65c55125e211205337fc3f50e7964b817117c3a1
Caroline Ninganga <carolninganga@gmail.com>
2020-06-12 15:55:28 -0400

    added Temperature component

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Temperature from "./components/Temperature";
+
+function App() {
+
+  return (
+      <React.Fragment>
+        <Temperature />;
+     </React.Fragment>
+  )
+}
+
+export default App;
